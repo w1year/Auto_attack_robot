c86ac92c5438d68573e7955e20d5b890f03abe63
Zvvln <164518626+w1year@users.noreply.github.com>
2026-01-22 21:08:00 +0800

    Add files via upload

---
+++ C++/build/CMakeFiles/RM_Auto_Attack.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/li/Desktop/C++/src/camera/mvs_camera.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/camera/mvs_camera.cpp.o"
+  "/home/li/Desktop/C++/src/can/can_protocol.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/can/can_protocol.cpp.o"
+  "/home/li/Desktop/C++/src/detection/yolo_detector.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/detection/yolo_detector.cpp.o"
+  "/home/li/Desktop/C++/src/detection/yolo_detector_tensorrt.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/detection/yolo_detector_tensorrt.cpp.o"
+  "/home/li/Desktop/C++/src/gimbal/gimbal_controller.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/gimbal/gimbal_controller.cpp.o"
+  "/home/li/Desktop/C++/src/main.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/main.cpp.o"
+  "/home/li/Desktop/C++/src/serial/serial_comm.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/serial/serial_comm.cpp.o"
+  "/home/li/Desktop/C++/src/utils/config.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/utils/config.cpp.o"
+  "/home/li/Desktop/C++/src/utils/logger.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/utils/logger.cpp.o"
+  "/home/li/Desktop/C++/src/utils/thread_optimizer.cpp" "/home/li/Desktop/C++/build/CMakeFiles/RM_Auto_Attack.dir/src/utils/thread_optimizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MVS_SDK_ENABLED"
+  "OPENCV_CUDA_ENABLED"
+  "TENSORRT_ENABLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../../MVS/MVS-3.0.1_aarch64_20241128/MVS/include"
+  "/usr/local/cuda/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/build/CMakeFiles/RM_Auto_Attack.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RM_Auto_Attack.dir/src/camera/mvs_camera.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/can/can_protocol.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/detection/yolo_detector.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/detection/yolo_detector_tensorrt.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/gimbal/gimbal_controller.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/main.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/serial/serial_comm.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/utils/config.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/utils/logger.cpp.o"
+  "CMakeFiles/RM_Auto_Attack.dir/src/utils/thread_optimizer.cpp.o"
+  "bin/RM_Auto_Attack"
+  "bin/RM_Auto_Attack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RM_Auto_Attack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
